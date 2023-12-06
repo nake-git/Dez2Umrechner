0d89d51ff7090b67686b5d2572081b0f2d1315e1
Oliver Nake <oliver.nake@web.de>
2023-12-06 20:46:25 +0100

    Projektdateien hinzufügen.

---
+++ Dez2Umrechner/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dez2Umrechner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Erstelle ein Programm, welches eine Dezimalzahl von der Konsole wahlweise in eine Binärzahl oder in eine Hexadezimalzahl umrechnet und auf der Konsole anzeigt.
+            // Nutze hierfür Strukturen wie Liste oder Array, Schleifen und Bedingungen
+            // Das Programm soll Fehleingaben abfangen können.
+            // Das Programm wird so oft durchgeführt bis der Nutzer es beenden will.
+            
+            // Bevor du mit der Programmierung beginnst, schreibe ein entsprechendes Flussdiagramm.
+
+            // Viel Erfolg.
+        }
+    }
+}
